763c53b464e78ac72f16e176d69f2b7634631bb8
purinmai <maaai.0307@gmail.com>
2020-11-17 17:41:37 +0900

    conditionモデル作成　バリデーションとアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,13 +3,15 @@ class Item < ApplicationRecord
   has_one :purchase
   has_one_attached :image
 
+  belongs_to_active_hash :category
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
   #ジャンルの選択が「--」の時は保存できないようにする
   validates :category_id, presence: true, numericality: { other_than: 1 }
 
-  belongs_to_active_hash :category
   belongs_to_active_hash :condition
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  validates :condition_id, presence: true, numericality: { other_than: 1 }
+
   belongs_to_active_hash :delivery_fee
   belongs_to_active_hash :days_to_sip
   belongs_to_active_hash :prefectures
