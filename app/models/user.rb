afc406e4747b02b25916601e2836833449d1129e
purinmai <maaai.0307@gmail.com>
2020-11-17 13:03:17 +0900

    共通のバリデーションをまとめる

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,27 +7,26 @@ class User < ApplicationRecord
   has_many :items
   has_many :purchases
 
-  validates :nickname, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+  end
 
-  
-  validates :password,presence: true, confirmation: true, length: { minimum: 6 , message: "6文字以上入力してください" },
-             format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{6,}\z/,
-             message: "は英数字混合で入力してください"} 
-  
-  with_options presence:true,
-    format: { with: /\A[ぁ-んァ-ン一-龥]+\z/,
-            message: 'は全角文字を使用してください' } do
+  validates :password, presence: true, confirmation: true, length: { minimum: 6, message: '6文字以上入力してください' },
+                       format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{6,}\z/,
+                                 message: 'は英数字混合で入力してください' }
+
+  with_options presence: true,
+               format: { with: /\A[ぁ-んァ-ン一-龥]+\z/,
+                         message: 'は全角文字を使用してください' } do
     validates :first_name
     validates :last_name
   end
 
   with_options  presence: true,
                 format: { with: /\A[ァ-ン]+\z/,
-                message: "は全角カナ文字を使用してください"  } do
-      validates :first_furigana
-      validates :last_furigana
+                          message: 'は全角カナ文字を使用してください' } do
+    validates :first_furigana
+    validates :last_furigana
   end
- 
-  validates :birthday, presence: true
- 
 end
