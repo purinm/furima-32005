ab44047b895478ecd80f36fa89bc787b3b145f39
purinmai <maaai.0307@gmail.com>
2020-11-17 17:18:09 +0900

    categoryモデル作成、バリデーションとアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  #ジャンルの選択が「--」の時は保存できないようにする
+  validates :category_id, presence: true, numericality: { other_than: 1 }
 
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
