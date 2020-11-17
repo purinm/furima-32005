045b640939d7c59dc16423f9896d20d360e699d9
purinmai <maaai.0307@gmail.com>
2020-11-17 13:04:11 +0900

    Rucbocop実行

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,16 +1,16 @@
 FactoryBot.define do
   password = Faker::Internet.password(min_length: 6)
-  
+
   factory :user do
     nickname { Faker::Name.last_name }
-    email { Faker::Internet.free_email}
-    
+    email { Faker::Internet.free_email }
+
     password { password }
     password_confirmation { password }
-    first_name { "山田" }
-    last_name {"太郎"}
-    first_furigana {"ヤマダ"}
-    last_furigana {"タロウ"}
-    birthday  {Faker::Date.between(from: '1930-01-01', to: '2015-12-31')}
+    first_name { '山田' }
+    last_name { '太郎' }
+    first_furigana { 'ヤマダ' }
+    last_furigana { 'タロウ' }
+    birthday { Faker::Date.between(from: '1930-01-01', to: '2015-12-31') }
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,10 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require "spec_helper"
-ENV["RAILS_ENV"] ||= "test"
-require File.expand_path("../config/environment", __dir__)
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
-require "rspec/rails"
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -30,7 +30,7 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
-I18n.locale = "en"
+I18n.locale = 'en'
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
