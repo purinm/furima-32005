681260205f8efcaa254f0cd46407a8b46db475e7
purinmai <maaai.0307@gmail.com>
2020-11-22 14:17:00 +0900

    商品編集機能の作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,24 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user.id
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else 
+      render "edit"
+    end
+  end
+
   private
 
   def item_params
